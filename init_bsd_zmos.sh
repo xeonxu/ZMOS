2ef24006c5cd8971ff862186009836310f73cecd
Noe Xu <xeonxu@gmail.com>
2020-07-14 13:43:22 +0800

    Update zmos install script.

--- init_bsd_zmos.sh
+++ init_bsd_zmos.sh
@@ -1,4 +1,4 @@
-#!/usr/local/bin/env bash
+#!/bin/sh
 # Usage: su
 # 	 ./init_bsd_zmos.sh
 
@@ -7,39 +7,28 @@ set -xeuo pipefail
 export USERNAME=$USER
 export PROXY_IP="192.168.0.182"
 
-function user_run {
-    sudo -u $USERNAME $0 $*
+user_run() {
+    sudo -u $USERNAME bash -c "$*"
 }
 
-# function root_mode {
-#     [ x`id -u` = x0 ] || {
-# 	echo "ERROR: You need to provide password to run as root!" >&2
-#         sudo $0 $*
-#         exit $?
-#     }
-# }
-
-function inst_ZMOS
+inst_ZMOS()
 {
-    # cd ~
-    # user_run git clone https://github.com/$USERNAME/ZMOS.git
-    # cd ~/ZMOS
+    #user_run git clone https://github.com/$USERNAME/ZMOS.git ~
+    #cd ~/ZMOS
     ./XDE/install-xde.sh
     user_run ./XDE/user-xde.sh
 }
 
-function inst_workbench
+inst_workbench()
 {
-    cd ~
-    git clone 192.168.0.103:~/Dropbox/Software/noesworkbench.git
-    cd noesworkbench
-    ./install_all.sh
+    user_run "cd && git clone xuzhiqiang@192.168.0.103:~/Dropbox/Software/noesworkbench.git"
+    user_run "cd ~/noesworkbench && ./inst_all.sh"
 }
 
 mkdir -p /usr/local/etc/pkg/repos
 cat > /usr/local/etc/pkg/repos/FreeBSD.conf <<EOF
 FreeBSD: {
-  url: "pkg+http://mirrors.ustc.edu.cn/freebsd-pkg/${ABI}/quarterly",
+  url: "pkg+http://mirrors.ustc.edu.cn/freebsd-pkg/\${ABI}/quarterly",
 }
 
 EOF
@@ -50,9 +39,10 @@ sed -i -e "s/socks4.*127.0.0.1.*9050$/socks5 	$PROXY_IP 1080/g" /usr/local/etc/p
 sed -i -e 's/#quiet_mode/quiet_mode/g' /usr/local/etc/proxychains.conf
 sed -i -e "s/^# %wheel ALL=(ALL) ALL$/%wheel ALL=(ALL) ALL/g" /usr/local/etc/sudoers
 
-pkg install -y git tmux bash zsh coreutils emacs-devel nerd-font sakura sbcl
+pkg install -y git tmux bash zsh coreutils emacs-devel nerd-fonts sakura sbcl
 
 user_run chsh -s /usr/local/bin/zsh
 
-user_run inst_workbench
 inst_ZMOS
+inst_workbench
+
